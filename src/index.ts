df664d4efed5f0e96932196e3db97fac0ed570e1
Martijn Dorsman <martijn.dorsman@gmail.com>
2024-10-13 23:39:27 +0200

    Refactor code to interact with Google Sheets API and perform CRUD operations
    This commit refactors the code in `index.ts` to interact with the Google Sheets API. It imports the necessary modules, initializes authentication, and creates a new instance of the `Sheets` class. It then inserts a record into the "Dogs" sheet and deletes a record from the "Users" sheet. This commit improves the functionality of the code by integrating it with the Google Sheets API.

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,14 @@
-(() => {
-  console.log("It works!");
-})();
+import { SPREADSHEET_ID } from "./constants.js";
+import { getAuth } from "./auth.js";
+import { Sheets } from "./Sheets.js";
+
+const auth = getAuth();
+
+const sheets = new Sheets(auth, SPREADSHEET_ID);
+console.log("Connecting to your spreadsheet database...");
+await sheets.insertInto("Dogs", { Name: "Johnny", Age: 25 });
+// await sheets.insertInto("Users", { Name: "Johnny", Age: 25 });
+console.log("Record added!");
+// await sheets.deleteFrom("Users", { where: { Name: "Martijn" } });
+// await sheets.deleteFrom("Dogs", { where: { Age: 10 } });
+console.log("Record deleted!");
